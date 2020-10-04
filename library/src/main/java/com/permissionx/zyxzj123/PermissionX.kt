dfc3be44ab3ef6cb11ac4ad6525ef24505a12c5c
zyxzj123 <1773925813@qq.com>
2020-10-04 21:06:00 +0800

    完成PermissionX库的开发与测试工作

---
+++ library/src/main/java/com/permissionx/zyxzj123/PermissionX.kt
@@ -0,0 +1,23 @@
+package com.permissionx.zyxzj123
+
+import androidx.fragment.app.FragmentActivity
+import java.security.Permissions
+
+object PermissionX {
+
+    private const val TAG = "InvisibleFragment"
+
+    fun request(activity: FragmentActivity, vararg permissions: String, callback: PermissionCallback) {
+        val fragmentManager = activity.supportFragmentManager
+        val existedFragment = fragmentManager.findFragmentByTag(TAG)
+        val fragment = if (existedFragment != null) {
+            existedFragment as InvisibleFragment
+        } else {
+            val invisibleFragment = InvisibleFragment()
+            fragmentManager.beginTransaction().add(invisibleFragment, TAG).commitNow()
+            invisibleFragment
+        }
+        fragment.requestNow(callback, *permissions)
+    }
+
+}
